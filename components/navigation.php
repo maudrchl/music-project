24d2ad5e84cd830870b1564d39f790266747a07d
maudrochel <rochel.maud@gmail.com>
2018-02-09 17:08:35 +0100

    initialize folder

---
+++ components/navigation.php
@@ -0,0 +1,14 @@
+<div class="navbar">
+    <div class="item">
+        <img src="assets/icones/network.svg" alt="network" width=35px>
+    </div>
+    <div class="item">
+        <img src="assets/icones/pad.svg" alt="pad" width=30px>
+    </div>
+    <div class="item">
+        <img src="assets/icones/avatar.svg" alt="profile" width=30px>
+    </div>
+    <div class="item">
+        <img src="assets/icones/settings.svg" alt="network" width=30px>
+    </div>
+</div>
