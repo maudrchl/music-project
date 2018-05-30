c86b39da23d9dbb123665a7d97a90e9754365462
maudrochel <rochel.maud@gmail.com>
2018-05-30 12:54:20 +0200

    💊  Merge

--- views/pages/social.php
+++ views/pages/social.php
@@ -5,7 +5,7 @@
 ?>
 
 
-<div class="container">
+<div class="items_socialpage">
     <div class="items">
         <?php
             include 'navigation.php';
@@ -14,10 +14,10 @@
     <h1>Your timeline</h1>
     <span class="name_login"><?= substr($_SESSION['user']['name'], 0, 45).'.' ?></span>
     <a href="logout"><img class="off" src="assets/img/power.svg" width=20></a>
-    <div class="items songs song1"></div>
-    <div class="items songs song2"></div>
-    <div class="items songs song3"></div>
-    <div class="items songs song4"></div>
+    <div class="items_social item_social1"></div>
+    <div class="items_social"></div>
+    <div class="items_social"></div>
+    <div class="items_social"></div>
 </div>
 </body>
 <script src="js/main.js"></script>
