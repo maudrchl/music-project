da0bbe203ea2a13140bbf07c897b5b42a613cbdb
maudrochel <rochel.maud@gmail.com>
2018-05-30 17:26:22 +0200

    🚶🏻 POO Pad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+if(typeof PadMusic == "function"){
+    const padMusic = new PadMusic()
+}
+
 if(typeof Settings == "function"){
     const settings = new Settings()
 }
--- views/pages/pad.php
+++ views/pages/pad.php
@@ -61,66 +61,66 @@
 
             <!--LEFT BEAT TOUCHES -->
             <div class="left_touches_container_beats beats">
-                <div class="touches touches_beat left_touche_beats key-192" data-sound="pad_1" ><a href="#"></a></div>
-                <div class="touches touches_beat left_touche_beats key-49" data-sound="pad_2"><a href="#"></a></div>
-                <div class="touches touches_beat left_touche_beats key-50" data-sound="pad_3"><a href="#"></a></div>
-                <div class="touches touches_beat left_touche_beats key-51" data-sound="pad_4"><a href="#"></a></div>
-                <div class="touches touches_beat left_touche_beats key-52" data-sound="pad_5"><a href="#"></a></div>
-                <div class="touches touches_beat left_touche_beats key-53" data-sound="pad_6"><a href="#"></a></div>
+                <div class="touches touches_beat left_touche_beats key-192" data-sound="sons/beats/1.wav" ><a href="#"></a></div>
+                <div class="touches touches_beat left_touche_beats key-49" data-sound="sons/beats/2.wav"><a href="#"></a></div>
+                <div class="touches touches_beat left_touche_beats key-50" data-sound="sons/beats/3.wav"><a href="#"></a></div>
+                <div class="touches touches_beat left_touche_beats key-51" data-sound="sons/beats/4.wav"><a href="#"></a></div>
+                <div class="touches touches_beat left_touche_beats key-52" data-sound="sons/beats/5.wav"><a href="#"></a></div>
+                <div class="touches touches_beat left_touche_beats key-53" data-sound="sons/beats/6.wav"><a href="#"></a></div>
             </div>
 
             <!--RIGHT BEAT TOUCHES -->
 
             <div class="right_touches_container_beats beats">
-            <div class="touches touches_beat right_touche_beats key-54" data-sound="pad_7"><a href="#"></a></div>
-            <div class="touches touches_beat right_touche_beats key-55" data-sound="pad_8"><a href="#"></a></div>
-            <div class="touches touches_beat right_touche_beats key-56" data-sound="pad_9"><a href="#"></a></div>
-            <div class="touches touches_beat right_touche_beats key-57" data-sound="pad_10"><a href="#"></a></div>
-            <div class="touches touches_beat right_touche_beats key-48" data-sound="pad_11"><a href="#"></a></div>
-            <div class="touches touches_beat right_touche_beats key-189" data-sound="pad_12"><a href="#"></a></div>
+            <div class="touches touches_beat right_touche_beats key-54" data-sound="sons/beats/7.wav"><a href="#"></a></div>
+            <div class="touches touches_beat right_touche_beats key-55" data-sound="sons/beats/8.wav"><a href="#"></a></div>
+            <div class="touches touches_beat right_touche_beats key-56" data-sound="sons/beats/9.wav"><a href="#"></a></div>
+            <div class="touches touches_beat right_touche_beats key-57" data-sound="sons/beats/10.wav"><a href="#"></a></div>
+            <div class="touches touches_beat right_touche_beats key-48" data-sound="sons/beats/11.wav"><a href="#"></a></div>
+            <div class="touches touches_beat right_touche_beats key-189" data-sound="sons/beats/12.wav"><a href="#"></a></div>
             </div>
 
             <!--LEFT BASS TOUCHES -->
 
             <div class="left_touches_container_bass bass">
-            <div class="touches touches_bass left_touche_bass key-65" data-sound="pad_13"><a href="#"></a></div>
-            <div class="touches touches_bass left_touche_bass key-90" data-sound="pad_14"><a href="#"></a></div>
-            <div class="touches touches_bass left_touche_bass key-69" data-sound="pad_15"><a href="#"></a></div>
-            <div class="touches touches_bass left_touche_bass key-82" data-sound="pad_16"><a href="#"></a></div>
-            <div class="touches touches_bass left_touche_bass key-84" data-sound="pad_17"><a href="#"></a></div>
-            <div class="touches touches_bass left_touche_bass key-89" data-sound="pad_18"><a href="#"></a></div>
+            <div class="touches touches_bass left_touche_bass key-65" data-sound="sons/bass/1.wav"><a href="#"></a></div>
+            <div class="touches touches_bass left_touche_bass key-90" data-sound="sons/bass/2.wav"><a href="#"></a></div>
+            <div class="touches touches_bass left_touche_bass key-69" data-sound="sons/bass/3.wav"><a href="#"></a></div>
+            <div class="touches touches_bass left_touche_bass key-82" data-sound="sons/bass/4.wav"><a href="#"></a></div>
+            <div class="touches touches_bass left_touche_bass key-84" data-sound="sons/bass/5.wav"><a href="#"></a></div>
+            <div class="touches touches_bass left_touche_bass key-89" data-sound="sons/bass/6.wav"><a href="#"></a></div>
             </div>
 
             <!--RIGHT BASS TOUCHES -->
 
             <div class="right_touches_container_bass bass">
-            <div class="touches touches_bass right_touche_bass key-85" data-sound="pad_19"><a href="#"></a></div>
-            <div class="touches touches_bass right_touche_bass key-73" data-sound="pad_20"><a href="#"></a></div>
-            <div class="touches touches_bass right_touche_bass key-79" data-sound="pad_21"><a href="#"></a></div>
-            <div class="touches touches_bass right_touche_bass key-80" data-sound="pad_22"><a href="#"></a></div>
-            <div class="touches touches_bass right_touche_bass key-229" data-sound="pad_23"><a href="#"></a></div>
-            <div class="touches touches_bass right_touche_bass key-221" data-sound="pad_24"><a href="#"></a></div>
+            <div class="touches touches_bass right_touche_bass key-85" data-sound="sons/bass/7.wav"><a href="#"></a></div>
+            <div class="touches touches_bass right_touche_bass key-73" data-sound="sons/bass/8.wav"><a href="#"></a></div>
+            <div class="touches touches_bass right_touche_bass key-79" data-sound="sons/bass/9.wav"><a href="#"></a></div>
+            <div class="touches touches_bass right_touche_bass key-80" data-sound="sons/bass/10.wav"><a href="#"></a></div>
+            <div class="touches touches_bass right_touche_bass key-229" data-sound="sons/bass/11.wav"><a href="#"></a></div>
+            <div class="touches touches_bass right_touche_bass key-221" data-sound="sons/bass/12.wav"><a href="#"></a></div>
                 
             </div>
 
             <!--LEFT MELODIES TOUCHES -->
             <div class="left_touches_container_melodies melodies">
-            <div class="touches touches_melodies left_touche_melodies key-81" data-sound="pad_25"><a href="#"></a></div>
-            <div class="touches touches_melodies left_touche_melodies key-83" data-sound="pad_26"><a href="#"></a></div>
-            <div class="touches touches_melodies left_touche_melodies key-68" data-sound="pad_27"><a href="#"></a></div>
-            <div class="touches touches_melodies left_touche_melodies key-70" data-sound="pad_28"><a href="#"></a></div>
-            <div class="touches touches_melodies left_touche_melodies key-71" data-sound="pad_29"><a href="#"></a></div>
-            <div class="touches touches_melodies left_touche_melodies key-72" data-sound="pad_30"><a href="#"></a></div>
+            <div class="touches touches_melodies left_touche_melodies key-81" data-sound="sons/melodies/1.wav"><a href="#"></a></div>
+            <div class="touches touches_melodies left_touche_melodies key-83" data-sound="sons/melodies/2.wav"><a href="#"></a></div>
+            <div class="touches touches_melodies left_touche_melodies key-68" data-sound="sons/melodies/3.wav"><a href="#"></a></div>
+            <div class="touches touches_melodies left_touche_melodies key-70" data-sound="sons/melodies/4.wav"><a href="#"></a></div>
+            <div class="touches touches_melodies left_touche_melodies key-71" data-sound="sons/melodies/5.wav"><a href="#"></a></div>
+            <div class="touches touches_melodies left_touche_melodies key-72" data-sound="sons/melodies/6.wav"><a href="#"></a></div>
             </div>
 
             <!--RIGHT MELODIES TOUCHES -->
             <div class="right_touches_container_melodies melodies">
-                <div class="touches touches_melodies right_touche_melodies key-74" data-sound="pad_31"><a href="#"></a></div>
-                <div class="touches touches_melodies right_touche_melodies key-75" data-sound="pad_32"><a href="#"></a></div>
-                <div class="touches touches_melodies right_touche_melodies key-76" data-sound="pad_33"><a href="#"></a></div>
-                <div class="touches touches_melodies right_touche_melodies key-77" data-sound="pad_34"><a href="#"></a></div>
-                <div class="touches touches_melodies right_touche_melodies key-222" data-sound="pad_35"><a href="#"></a></div>
-                <div class="touches touches_melodies right_touche_melodies key-220" data-sound="pad_36"><a href="#"></a></div>
+                <div class="touches touches_melodies right_touche_melodies key-74" data-sound="sons/melodies/7.wav"><a href="#"></a></div>
+                <div class="touches touches_melodies right_touche_melodies key-75" data-sound="sons/melodies/8.wav"><a href="#"></a></div>
+                <div class="touches touches_melodies right_touche_melodies key-76" data-sound="sons/melodies/8.wav"><a href="#"></a></div>
+                <div class="touches touches_melodies right_touche_melodies key-77" data-sound="sons/melodies/9.wav"><a href="#"></a></div>
+                <div class="touches touches_melodies right_touche_melodies key-222" data-sound="sons/melodies/10.wav"><a href="#"></a></div>
+                <div class="touches touches_melodies right_touche_melodies key-220" data-sound="sons/melodies/11.wav"><a href="#"></a></div>
             </div>
         </div>
 
@@ -137,6 +137,7 @@
     </div>
     </div>
     </body>
-    <script src="js/pad.js"></script>
+    <script src="js/PadMusic.js"></script>
+    <script src="js/main.js"></script>
 
     </html>
