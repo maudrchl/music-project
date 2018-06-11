dabd786ef02dbeeb9ff2b1e51f62ac9def111267
maudrochel <rochel.maud@gmail.com>
2018-06-11 17:26:23 +0200

    💊 Errors

--- views/pages/social.php
+++ views/pages/social.php
@@ -17,37 +17,33 @@
     echo '<pre>';
     print_r($posts);
     echo '<pre>';
+
+    function get_username($pdo, $id) {
+        $query = $pdo->query("SELECT * FROM users WHERE id = $id");
+        $username = $query->fetch();
+        return $username->name;
+    }
     
 ?>
 
 <div class="items_socialpage">
 <?php echo $_SESSION["user"]; ?>
-    <div class="items">
-        <?php
-            include 'navigation.php';
-        ?>
-    </div>
     <h1 class="social_title">Your timeline</h1>
     <a href="logout"><img class="off" src="assets/img/power.svg" width=20></a>
-    <?php echo "<script>console.log(".var_dump($posts).")</script>" ?>
+
     <?php foreach($posts as $_post): ?>
-    <?php foreach($followers as $_follower): ?>
         <div class="items_social">
             <img class="pic_home" src="https://api.adorable.io/avatars/240/<?= $_post->user_id?>" width="100" height="100" alt="">
             <div class="social_content">
-                <?php foreach($users as $_user): ?>
-                <?php if($_user->id === $_post->user_id){?>
-                    <h2 class="post">user name : <?= $_user->name ?></h2>
+                <h2 class="post">user name : <?= get_username($pdo, $_post->user_id) ?></h2>
                 <h2 class="post">user id : <?= $_post->user_id ?></h2>
                 <h2 class="post">message : <?= $_post->body ?></h2>
                 <h2 class="post">date : <?= $_post->stamp ?></h2>
-                <?php } ?>
-                <?php endforeach; ?>
-            <div>
+            </div>
         </div>
     <?php endforeach; ?>
-    <?php endforeach; ?>
 </div>
+
 </body>
 <script src="js/main.js"></script>
 </html>
